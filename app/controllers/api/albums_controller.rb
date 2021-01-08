e2836b6db474df1b921e76c8a8e90f101a3bb255
JlordA <james.ardery@gmail.com>
2021-01-07 23:28:50 -0500

    create function added to albums controller

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -5,4 +5,13 @@ class Api::AlbumsController < ApplicationController
         render json: albums
     end
 
+    def create
+        album = Album.create(album_params)
+        render json: album
+    end
+
+    private
+    def album_params
+        params.permit(:title, :artist, :year, :url, :thumb, :wants)
+    end
 end
