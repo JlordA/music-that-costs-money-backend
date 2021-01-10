b63d19f96186cbf1337b915e7d60bb88bb7cfa1d
JlordA <james.ardery@gmail.com>
2021-01-09 21:51:19 -0500

    post controller actions

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,19 @@
 class Api::PostsController < ApplicationController
+
+    def index
+        posts = Post.all
+        render json: posts
+    end
+
+    def create
+        post = Post.create(post_params)
+        render json: post
+    end
+
+    private
+
+    def post_params
+        params.permit(:title, :body, :price, :like, :for_sale)
+    end
+
 end
